5e8cd68e7a79bcdaa004112d4891a6adb8883381
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-09-20 00:05:08 +0500

    new commit

---
+++ Learning JS/Class 1/app.js
@@ -0,0 +1,73 @@
+// data types
+
+// "hello world"  // string data type
+// 1, 2, 3 // intiger data type
+// 1.3 // float data type
+
+// alert(hello)
+
+
+
+
+// var abc = 'mohammad saad ali'
+
+// alert(abc)
+
+// document.write('<h1>'+'hello world'+'</h1>')
+
+// var num1 = +prompt ('Enter value 1')
+// var num2 = +prompt ('Enter value 2')
+// var add = num1 + num2
+// var subtract = num1 - num2 
+// var multiply = num1 * num2
+// var divide = num1 / num2
+// var modulus = num1 % num2
+
+// document.write (
+//     "<h1>" + "<center>" + "<table border= '1px'>" 
+//     + "<tr>" 
+//     + "<th>" + "Addition" + "</th>" 
+//     + "<th>" + "Subtraction" + "</th>" 
+//     + "<th>" + "Multiplication" + "</th>" 
+//     + "<th>" + "Dividing" + "</th>" 
+//     + "<th>" + "Modulus" + "</th>" 
+//     + "</tr>"
+
+//     + "<tr>"
+//     + "<td>" + add + "</td>"
+//     + "<td>" + subtract + "</td>"
+//     + "<td>" + multiply + "</td>"
+//     + "<td>" + divide + "</td>"
+//     + "<td>" + modulus + "</td>"
+//     +  "</tr>"
+//     + "</table>"
+//     + "</center>"
+//     + "</h1>"
+// )
+
+// var message = ('Thanks for submitting your form')
+
+// alert (message)
+// var Num = (2 * 2) * (2 + 2) * (2 + 4)
+
+// alert (Num)
+
+
+// var x = prompt ("where does the dog live?")
+// if (x === "karachi") {
+//     alert ("correct!")
+// }
+// // if (x !== "karachi") { 
+// //     alert ("wrong answer")
+// // } 
+// else if (x === "punjab") {
+//     alert ("Incorrect but close")
+// }
+
+// else {
+//      alert("Incorrect");
+// }
+
+
+
+
