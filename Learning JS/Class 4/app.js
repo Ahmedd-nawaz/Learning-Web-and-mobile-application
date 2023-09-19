e8d6acfa13289082b71a9fd0e5cfb5c62b5bde6b
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-09-20 00:07:34 +0500

    new commit

---
+++ Learning JS/Class 4/app.js
@@ -0,0 +1,33 @@
+var arr = ['Saad', 'Ali', 24, true];
+
+document.write(arr)
+
+
+
+
+// arr.splice(3,1,"Hello World", 'Ali 2') // Added ali 2 in the last
+// arr.splice(3,1,"Hello world") // Remove last element and add Hello world in the last 
+// arr.splice(3,1)  // Remove last element
+// arr.splice(2,2,"abc") // it can remove 2 elements and add abc 
+// arr.splice(2,1,"abc") // can remove one element and add abc it can add abc on 24
+// arr.splice(2,1) // Start from 2 and remove one element it can remove 24  
+// arr.splice(2,0) // cannot delete any element
+// arr.splice(0,2) // can remove 0 to 2 elements 
+// arr.splice(0,1) // can remove 1st emelent 
+// arr.splice(0,0)
+// arr.unshift('Esha', 'Rehman')
+// arr.shift()
+// arr.push('hamza', 'Qari sahb')
+// arr.pop()
+// arr.pop()
+// document.write(arr[0]+" "+arr[2])
+
+// var arr2 = arr.slice(0)
+// console.log(arr2)
+
+// var userInput = +prompt("Table No");
+// var userInput2 = +prompt("Value");
+
+// for(var i=1;i<=userInput2;i++){
+//     document.write("<table border=1px>"+"<td>"+userInput+" "+ "x"+" "+ i +" = " +userInput*i+ "</td>" +"</table>")
+// }
