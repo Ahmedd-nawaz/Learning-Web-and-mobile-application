4d0af2e646b231c8ba9c64b722ef5a5adfb1ba3d
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-09-26 16:52:48 +0500

    new commit

---
+++ Learning JS/Class 6/app.js
@@ -0,0 +1,52 @@
+// var userInput = prompt("Enter city")
+// var arry = ["karachi", "Lahore", "Islamabad", "Peshawar", "Quetta"]
+// var match = false
+
+// for (var i = 0; i <= arry.length; i++) {
+
+//     if (userInput == arry[i]) {
+//         match = true
+//         alert("city found")
+//         break
+//     }
+// }
+// if (match == false) {
+//     alert("city not found")
+// }
+
+// var arr = ["Salman", "Arbaz", "Sohail"];
+// var arr2 = ["Saleem khan", "Asad"]
+
+// for (var i = 0; i < arr.length; i++){
+//     for(var j = 0; j < arr2.length; j++){
+//         document.write(arr[i] + " " + arr2[j] + "<br>")
+//     }
+// }
+
+// for (var i = 1; i <= 100; i = i + 10) {
+//     for (var j = i; j < i + 10; j++) {
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// for (var i = 101; i <= 200; i = i + 10) {
+//     for (var j = i; j < i + 10; j++) {
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
+
+// var arr = ["BluRay","Upchuck","Lojack","Gizmo","Do-Rag"];
+// var arr2 = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// var match = false;
+
+// for (var i = 0; i < arr.length; i++){
+    // for(var j=0;j< arr2.length; j++){
+        // match=true
+        // document.write("<table border=1px>", "<tr>","<td>" , arr[i], " ", arr2[j] ,"</td>", "</tr>", "</table>")
+    // }
+    // document.write("<br>")
+// }
+
