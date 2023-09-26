f6e28e69e3d7613793be4e71288a7bed01db9ac7
ahmedd-nawaz <ahmednawaz3926@gmail.com>
2023-09-26 16:53:37 +0500

    new commit

---
+++ Learning JS/Class 7/app.js
@@ -0,0 +1,55 @@
+// var userInput = prompt("Enter city");
+// var pro = userInput.slice(0, 1).toUpperCase();
+// var pro2 = userInput.slice(1).toLowerCase();
+// var finalUserInput = pro + pro2;
+// var array = ["Karachi", "Lahore", "Islamabad", "Peshawar", "Quetta", "Landhi"];
+// var abc = false;
+
+// for (var i = 0; i <= array.length; i++) {
+
+//     if (finalUserInput == array[i]) {
+//         abc = true
+//         alert("City Found");
+//         break
+//     }
+// };
+// if (abc == false) {
+//     alert("City Not Found ")
+// };
+
+// var arr = ["Saad", "Osama", "Taha", "Bilal", "Saad"]
+// // document.write(arr.indexOf("Saad",1))
+// document.write(arr.lastIndexOf("Saad"))
+
+// charAt
+
+// var naam = "RED"
+// document.write(naam.charAt(1))
+
+
+// Math.round
+// var percentage = 72.2
+    // document.write(Math.round(percentage))  // Result percentage sum up in 72
+
+//  Math.ceil 
+// var percentage = 72.2
+//     document.write(Math.ceil(percentage))  // Result percentege sum up in 73
+
+// Math.floor
+// var percentage = 72.2
+//     document.write(Math.floor(percentage)) // Result percentage sum up in 72
+
+// Math.random
+// console.log(Math.random() * 10)  // Result rendom numbers
+
+// Head Tail Game
+// var player1 = prompt("Enter Heads Name");
+// var player2 = prompt("Enter Tails Name");
+// var toss = Math.random() * 2;
+// var change = Math.floor(toss)
+// if(change == 0){
+//     document.write(player1, " Heads Win")
+// }
+// else{
+//     document.write(player2," Tails win")
+// }
